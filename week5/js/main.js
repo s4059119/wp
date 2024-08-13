24582219f12c4fc68a5b7e8d2e90114c3d18b59b
Tanya Unterberger <e102767@rmit.edu.au>
2024-08-13 16:47:48 +1000

    added week 5 exercises

---
+++ week5/js/main.js
@@ -0,0 +1,14 @@
+
+let msg = 'Hello, World!';
+
+msg = "Hello, Tanya's class!";
+
+let intExample = 42;
+let dbl = 42.42;
+let str = 'Hello, World!';
+let bool = true;
+let x = '1';
+//x = 1;
+
+console.log(intExample + x);
+console.log(intExample + dbl);

7f0fd62015ee8d8ef7c7af5371c360fbf81357e2
s4059119 <s4059119@student.rmit.edu.au>
2024-09-30 13:18:38 +1000

    Initial code for pets.php

---
+++ a2/pets.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once('includes/db_connect.inc');
+
+
+$title = "Pets Page";
+
+
+include_once('includes/header.inc');
+?>
+
+<header>
+    <?php
+
+    include_once('includes/nav.inc');
+    ?>

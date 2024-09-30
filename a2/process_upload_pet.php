e97f8619c6be5ef675dcf9f5810af84aaa464827
s4059119 <s4059119@student.rmit.edu.au>
2024-09-30 16:23:58 +1000

    Add initial code

--- a2/process_upload_pet.php
+++ a2/process_upload_pet.php
@@ -1,1 +1,16 @@
+<?php
 
+require_once('includes/db_connect.inc');
+
+
+function validateInput($str) {
+    return trim($str);
+}
+
+
+function sanitizeFileName($str) {
+   
+    $str = preg_replace('/[^a-zA-Z0-9_-]/', '', $str);
+    
+    return substr($str, 0, 50);
+}

7b06dbe865176ab391313665a4bd65e80d8cffc4
s4059119 <s4059119@student.rmit.edu.au>
2024-09-30 16:12:12 +1000

    Include header and footer in add.php

--- a2/add.php
+++ a2/add.php
@@ -1,1 +1,19 @@
+<?php
+$title = "Add Page";
 
+include_once('includes/header.inc');
+?>
+
+<header>
+    <?php
+    include_once('includes/nav.inc');
+    ?>
+    <h4>Add a Pet</h4>
+    <p class="p2">You can add a new pet here</p>
+</header>
+
+
+
+<?php
+include_once('includes/footer.inc');
+?>

a5768b8bbf35b9400fd831cc295282aee9f8bb6e
Harneet <s4059119@student.rmit.edu.au>
2024-11-06 17:36:37 +1100

    Fetch distinct pet types for filter dropdown in gallery.php

--- a3/gallery.php
+++ a3/gallery.php
@@ -4,3 +4,7 @@ session_start();
 include('includes/header.inc');
 include('includes/nav.inc');
 include('includes/db_connect.inc');
+
+// Fetch all distinct types from the pets table
+$type_sql = "SELECT DISTINCT type FROM pets";
+$type_result = mysqli_query($conn, $type_sql);

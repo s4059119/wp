e1961799d7fd27fe2f921bfb8014ffd47c2d0d96
Harneet <s4059119@student.rmit.edu.au>
2024-11-06 17:37:10 +1100

    Retrieve pets based on selected type in gallery.php

--- a3/gallery.php
+++ a3/gallery.php
@@ -8,3 +8,17 @@ include('includes/db_connect.inc');
 // Fetch all distinct types from the pets table
 $type_sql = "SELECT DISTINCT type FROM pets";
 $type_result = mysqli_query($conn, $type_sql);
+
+// Fetch all pets for display or based on selected type
+$selected_type = isset($_GET['type']) ? $_GET['type'] : '';
+$pet_sql = "SELECT petid, petname, image, caption FROM pets"; // Changed 'name' to 'petname'
+if ($selected_type) {
+    $pet_sql .= " WHERE type = ?";
+    $stmt = $conn->prepare($pet_sql);
+    $stmt->bind_param("s", $selected_type);
+    $stmt->execute();
+    $result = $stmt->get_result();
+} else {
+    $result = mysqli_query($conn, $pet_sql);
+}
+?>

10e83ebf2414b69f59efd60fc3476845e5b2a91b
Harneet <s4059119@student.rmit.edu.au>
2024-11-06 18:04:50 +1100

    Display success message for logged-in users in index.php

--- a3/index.php
+++ a3/index.php
@@ -4,3 +4,11 @@ session_start();
 include('includes/header.inc');
 include('includes/nav.inc');
 include("includes/db_connect.inc");
+
+if (isset($_SESSION['usrmsg'])): ?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <?= htmlspecialchars($_SESSION['usrmsg']) ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <?php unset($_SESSION['usrmsg']); ?>
+<?php endif; ?>

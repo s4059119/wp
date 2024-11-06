a9f5ba3f08b2e265f4417dfc5b423b6bf0733600
Harneet <s4059119@student.rmit.edu.au>
2024-11-06 18:03:50 +1100

    Initialize session, include header, nav, and db connection in index.php

--- a3/index.php
+++ a3/index.php
@@ -1,25 +1,6 @@
 <?php
+session_start();
 
-$title = "Homepage Page";
-
-include_once('includes/header.inc');
-?>
-
-<header>
-    <?php
-
-    include_once('includes/nav.inc');
-    ?>
-</header>
-
-<main class="home-main">
-    <aside>
-        <img src="images/main.jpg" alt="main image" class="bordered-image">
-    </aside>
-    <h1 id="h1">PETS VICTORIA</h1>
-    <h2 id="h2">WELCOME TO PET ADOPTION</h2>
-</main>
-
-<?php
-include_once('includes/footer.inc');
-?>
+include('includes/header.inc');
+include('includes/nav.inc');
+include("includes/db_connect.inc");

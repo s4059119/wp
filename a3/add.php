4b9193e9b4e5640b1dd623d648d6223038be82d9
Harneet <s4059119@student.rmit.edu.au>
2024-11-06 14:53:19 +1100

    Check if user is logged in in add.php

--- a3/add.php
+++ a3/add.php
@@ -1,3 +1,10 @@
 <?php
 session_start();
 
+
+// Check if the user is logged in by verifying the session variable
+if (!isset($_SESSION['username'])) {
+    // Redirect to login page if not logged in
+    header("Location: login.php");
+    exit();
+}

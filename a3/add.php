037c97ae33ae49f20cffa63d2d7217cfbae7e69d
Harneet <s4059119@student.rmit.edu.au>
2024-11-06 14:54:19 +1100

    Add includes for header and navigation in add.php

--- a3/add.php
+++ a3/add.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 
-
 // Check if the user is logged in by verifying the session variable
 if (!isset($_SESSION['username'])) {
     // Redirect to login page if not logged in
     header("Location: login.php");
     exit();
 }
+
+include('includes/header.inc');
+include('includes/nav.inc');

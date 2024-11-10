ab25ec0caa3c03f9d924bc2c531288cea1c0a9d2
Harneet <s4059119@student.rmit.edu.au>
2024-11-11 01:35:19 +1100

    Connect to database and retrieve pet ID in delete.php

--- a3/delete.php
+++ a3/delete.php
@@ -5,3 +5,12 @@ if (!isset($_SESSION['username'])) {
     header("Location: login.php");
     exit();
 }
+
+include('includes/db_connect.inc');
+
+// Get the pet ID
+$id = $_GET['id'] ?? null;
+if (!$id || !is_numeric($id)) {
+    echo "Invalid pet ID!";
+    exit();
+}

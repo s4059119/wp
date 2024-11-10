385fcd9cd2830b494d621a0123c1263a43723399
Harneet <s4059119@student.rmit.edu.au>
2024-11-11 02:07:56 +1100

    Add main structure and loop to display pets in user.php

--- a3/user.php
+++ a3/user.php
@@ -19,3 +19,18 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 ?>
+
+<main>
+    <h1 class="userh1"><?= htmlspecialchars($username) ?>'s Collection</h1>
+    
+    <?php
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            ?>
+            <div class="petCard">
+                <img class="detailsImg" src="images/<?= htmlspecialchars($row['image']) ?>" alt="<?= htmlspecialchars($row['petname']) ?>">
+                
+                <div class="detailsTextContainer">
+                    <p class="detailsName"><?= htmlspecialchars($row['petname']) ?></p>
+                    <p class="detailsDescription"><?= htmlspecialchars($row['description']) ?></p>
+                </div>

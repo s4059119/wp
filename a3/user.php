6ef4da5b5c46329b2daa942d07465dbd698f5ff6
Harneet <s4059119@student.rmit.edu.au>
2024-11-11 02:06:38 +1100

    Add login check and error message for non-logged-in users in user.php

--- a3/user.php
+++ a3/user.php
@@ -4,3 +4,10 @@ session_start();
 include('includes/db_connect.inc');
 include('includes/header.inc');
 include('includes/nav.inc');
+
+if (!isset($_SESSION['username'])) {
+    echo "<p>You must be logged in to view this page.</p>";
+    exit;
+}
+
+$username = $_SESSION['username']; // Get the logged-in username

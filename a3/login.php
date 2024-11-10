505d6002a8956bba5844aee9eeddd7a16b87e209
Harneet <s4059119@student.rmit.edu.au>
2024-11-11 01:57:42 +1100

    Add redirect for already logged-in users in login.php

--- a3/login.php
+++ a3/login.php
@@ -4,3 +4,9 @@ session_start();
 include('includes/header.inc');
 include('includes/nav.inc');
 include("includes/db_connect.inc");
+
+// Redirect if already logged in
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+    exit();
+}

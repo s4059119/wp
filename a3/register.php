72e585bdbdbcc75fadec4ab580ea0a0951815911
Harneet <s4059119@student.rmit.edu.au>
2024-11-11 02:23:54 +1100

    Add registration form and footer include in register.php

--- a3/register.php
+++ a3/register.php
@@ -39,3 +39,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $conn->close();
 ?>
+
+<main>
+    <div class="container">
+        <h3>Register</h3>
+        <form action="register.php" method="post">
+            <div class="mb-3 mt-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" name="username" id="username" class="form-control w-50" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" id="password" class="form-control w-50" required>
+            </div>
+            <div class="mb-3">
+                <input type="submit" class="btn btn-primary" value="Register">
+            </div>
+        </form>
+    </div>
+</main>
+
+<?php
+include('includes/footer.inc');
+?>
